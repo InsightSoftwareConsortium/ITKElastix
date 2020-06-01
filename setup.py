fe7b1219003353a75ca03f221c18ec8f6abe97e2
Matt McCormick <matt.mccormick@kitware.com>
2020-06-01 11:46:05 -0400

    ENH: Bump Python package version to 0.6.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if 'ELASTIX_USE_OPENCL' in os.environ:
 
 setup(
     name=package_name,
-    version='0.6.0',
+    version='0.6.1',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
@@ -49,6 +49,6 @@ setup(
     keywords='ITK InsightToolkit',
     url=r'https://itk.org/',
     install_requires=[
-        r'itk>=5.1.0'
+        r'itk>=5.1.0.post2'
     ]
     )
