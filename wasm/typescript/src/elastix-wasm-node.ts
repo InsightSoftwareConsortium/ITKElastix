18c028dd262485ca226251bdc98dd37ad37173ac
Matt McCormick <matt.mccormick@kitware.com>
2023-08-26 17:39:43 -0400

    ENH: Bump itk-wasm version 1.0.0-b.130

--- wasm/typescript/src/elastix-wasm-node.ts
+++ wasm/typescript/src/elastix-wasm-node.ts
@@ -1,4 +1,4 @@
-// Generated file. Do not edit.
+// Generated file. To retain edits, remove this comment.
 
 import {
   Image,
@@ -17,6 +17,7 @@ import path from 'path'
 /**
  * Rigid and non-rigid registration of images.
  *
+ * @param {ElastixWasmOptions} options - options object
  *
  * @returns {Promise<ElastixWasmNodeResult>} - result object
  */
@@ -27,24 +28,29 @@ async function elastixWasmNode(
   const desiredOutputs: Array<PipelineOutput> = [
     { type: InterfaceTypes.Image },
   ]
+
   const inputs: Array<PipelineInput> = [
   ]
 
   const args = []
   // Inputs
   // Outputs
-  args.push('0')
+  const resultName = '0'
+  args.push(resultName)
+
   // Options
   args.push('--memory-io')
   if (typeof options.fixed !== "undefined") {
     const inputCountString = inputs.length.toString()
     inputs.push({ type: InterfaceTypes.Image, data: options.fixed as Image })
     args.push('--fixed', inputCountString)
+
   }
   if (typeof options.moving !== "undefined") {
     const inputCountString = inputs.length.toString()
     inputs.push({ type: InterfaceTypes.Image, data: options.moving as Image })
     args.push('--moving', inputCountString)
+
   }
 
   const pipelinePath = path.join(path.dirname(import.meta.url.substring(7)), '..', 'pipelines', 'elastix-wasm')
--- wasm/typescript/src/elastix-wasm.ts
+++ wasm/typescript/src/elastix-wasm.ts
@@ -1,4 +1,4 @@
-// Generated file. Do not edit.
+// Generated file. To retain edits, remove this comment.
 
 import {
   Image,
@@ -13,13 +13,12 @@ import ElastixWasmResult from './elastix-wasm-result.js'
 
 
 import { getPipelinesBaseUrl } from './pipelines-base-url.js'
-
-
 import { getPipelineWorkerUrl } from './pipeline-worker-url.js'
 
 /**
  * Rigid and non-rigid registration of images.
  *
+ * @param {ElastixWasmOptions} options - options object
  *
  * @returns {Promise<ElastixWasmResult>} - result object
  */
@@ -31,24 +30,29 @@ async function elastixWasm(
   const desiredOutputs: Array<PipelineOutput> = [
     { type: InterfaceTypes.Image },
   ]
+
   const inputs: Array<PipelineInput> = [
   ]
 
   const args = []
   // Inputs
   // Outputs
-  args.push('0')
+  const resultName = '0'
+  args.push(resultName)
+
   // Options
   args.push('--memory-io')
   if (typeof options.fixed !== "undefined") {
     const inputCountString = inputs.length.toString()
     inputs.push({ type: InterfaceTypes.Image, data: options.fixed as Image })
     args.push('--fixed', inputCountString)
+
   }
   if (typeof options.moving !== "undefined") {
     const inputCountString = inputs.length.toString()
     inputs.push({ type: InterfaceTypes.Image, data: options.moving as Image })
     args.push('--moving', inputCountString)
+
   }
 
   const pipelinePath = 'elastix-wasm'
