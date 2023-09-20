29b0e2ffbfb2dd2dbf05f688b52acffa05bb66e5
Matt McCormick <matt.mccormick@kitware.com>
2023-09-20 16:17:27 -0400

    ENH: wasm demo app controll, load-sample-input modules

---
+++ wasm/typescript/test/browser/demo-app/default-parameter-map-controller.ts
@@ -0,0 +1,133 @@
+// Generated file. To retain edits, remove this comment.
+
+import * as elastix from '../../../dist/bundles/elastix.js'
+import defaultParameterMapLoadSampleInputs, { usePreRun } from "./default-parameter-map-load-sample-inputs.js"
+
+class DefaultParameterMapModel {
+
+  inputs: Map<string, any>
+  options: Map<string, any>
+  outputs: Map<string, any>
+
+  constructor() {
+    this.inputs = new Map()
+    this.options = new Map()
+    this.outputs = new Map()
+    }
+  }
+
+
+class DefaultParameterMapController  {
+
+  constructor(loadSampleInputs) {
+    this.loadSampleInputs = loadSampleInputs
+
+    this.model = new DefaultParameterMapModel()
+    const model = this.model
+
+    this.webWorker = null
+
+    if (loadSampleInputs) {
+      const loadSampleInputsButton = document.querySelector("#defaultParameterMapInputs [name=loadSampleInputs]")
+      loadSampleInputsButton.setAttribute('style', 'display: block-inline;')
+      loadSampleInputsButton.addEventListener('click', async (event) => {
+        loadSampleInputsButton.loading = true
+        await loadSampleInputs(model)
+        loadSampleInputsButton.loading = false
+      })
+    }
+
+    // ----------------------------------------------
+    // Inputs
+    const transformNameElement = document.querySelector('#defaultParameterMapInputs sl-input[name=transform-name]')
+    transformNameElement.addEventListener('sl-change', (event) => {
+        model.inputs.set("transformName", transformNameElement.value)
+    })
+
+    // ----------------------------------------------
+    // Options
+    const numberOfResolutionsElement = document.querySelector('#defaultParameterMapInputs sl-input[name=number-of-resolutions]')
+    numberOfResolutionsElement.addEventListener('sl-change', (event) => {
+        model.options.set("numberOfResolutions", parseInt(numberOfResolutionsElement.value))
+    })
+
+    const finalGridSpacingElement = document.querySelector('#defaultParameterMapInputs sl-input[name=final-grid-spacing]')
+    finalGridSpacingElement.addEventListener('sl-change', (event) => {
+        model.options.set("finalGridSpacing", parseFloat(finalGridSpacingElement.value))
+    })
+
+    // ----------------------------------------------
+    // Outputs
+    const parameterObjectOutputDownload = document.querySelector('#defaultParameterMapOutputs sl-button[name=parameter-object-download]')
+    parameterObjectOutputDownload.addEventListener('click', async (event) => {
+        event.preventDefault()
+        event.stopPropagation()
+        if (model.outputs.has("parameterObject")) {
+            const fileName = `parameterObject.json`
+            globalThis.downloadFile(new TextEncoder().encode(JSON.stringify(model.outputs.get("parameterObject"))), fileName)
+        }
+    })
+
+    const tabGroup = document.querySelector('sl-tab-group')
+    tabGroup.addEventListener('sl-tab-show', async (event) => {
+      if (event.detail.name === 'defaultParameterMap-panel') {
+        const params = new URLSearchParams(window.location.search)
+        if (!params.has('functionName') || params.get('functionName') !== 'defaultParameterMap') {
+          params.set('functionName', 'defaultParameterMap')
+          const url = new URL(document.location)
+          url.search = params
+          window.history.replaceState({ functionName: 'defaultParameterMap' }, '', url)
+        }
+        if (!this.webWorker && loadSampleInputs && usePreRun) {
+          await loadSampleInputs(model, true)
+          await this.run()
+        }
+      }
+    })
+
+    const runButton = document.querySelector('#defaultParameterMapInputs sl-button[name="run"]')
+    runButton.addEventListener('click', async (event) => {
+      event.preventDefault()
+
+      if(!model.inputs.has('transformName')) {
+        globalThis.notify("Required input not provided", "transformName", "danger", "exclamation-octagon")
+        return
+      }
+
+
+      try {
+        runButton.loading = true
+
+        const t0 = performance.now()
+        const { parameterObject, } = await this.run()
+        const t1 = performance.now()
+        globalThis.notify("defaultParameterMap successfully completed", `in ${t1 - t0} milliseconds.`, "success", "rocket-fill")
+
+        model.outputs.set("parameterObject", parameterObject)
+        parameterObjectOutputDownload.variant = "success"
+        parameterObjectOutputDownload.disabled = false
+        const parameterObjectDetails = document.getElementById("defaultParameterMap-parameter-object-details")
+        parameterObjectDetails.innerHTML = `<pre>${globalThis.escapeHtml(JSON.stringify(parameterObject, globalThis.interfaceTypeJsonReplacer, 2))}</pre>`
+        parameterObjectDetails.disabled = false
+        const parameterObjectOutput = document.getElementById("defaultParameterMap-parameter-object-details")
+      } catch (error) {
+        globalThis.notify("Error while running pipeline", error.toString(), "danger", "exclamation-octagon")
+        throw error
+      } finally {
+        runButton.loading = false
+      }
+    })
+  }
+
+  async run() {
+    const { webWorker, parameterObject, } = await elastix.defaultParameterMap(this.webWorker,
+      this.model.inputs.get('transformName'),
+      Object.fromEntries(this.model.options.entries())
+    )
+    this.webWorker = webWorker
+
+    return { parameterObject, }
+  }
+}
+
+const defaultParameterMapController = new DefaultParameterMapController(defaultParameterMapLoadSampleInputs)
---
+++ wasm/typescript/test/browser/demo-app/write-parameter-files-load-sample-inputs.ts
@@ -0,0 +1,28 @@
+// Generated file. To retain edits, remove this comment.
+
+export default null
+// export default async function writeParameterFilesLoadSampleInputs (model) {
+
+  // Load sample inputs for the writeParameterFiles function.
+  //
+  // This function should load sample inputs:
+  //
+  //  1) In the provided model map.
+  //  2) Into the corresponding HTML input elements if preRun is not true.
+  //
+  // Example for an input named `exampleInput`:
+
+  // const exampleInput = 5
+  // model.inputs.set("exampleInput", exampleInput)
+  // if (!preRun) {
+  //   const exampleElement = document.querySelector("#writeParameterFilesInputs [name=example-input]")
+  //   exampleElement.value = 5
+  // }
+
+  // return model
+// }
+
+// Use this function to run the pipeline when this tab group is select.
+// This will load the web worker if it is not already loaded, download the wasm module, and allocate memory in the wasm model.
+// Set this to `false` if sample inputs are very large or sample pipeline computation is long.
+export const usePreRun = true
