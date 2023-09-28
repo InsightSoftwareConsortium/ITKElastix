441ea3b5fd258d4542ef89ae93e27c0c6c6e9877
Matt McCormick <matt.mccormick@kitware.com>
2023-09-28 16:23:19 -0400

    ENH: Add test_write_parameter_files.py wasi test

---
+++ wasm/python/itkwasm-elastix-wasi/tests/test_write_parameter_files.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from dataclasses import asdict
+import json
+
+import pytest
+
+from itkwasm_compare_images import compare_images
+from itkwasm import Image
+import itk
+
+from itkwasm_elastix_wasi import write_parameter_files
+
+def test_write_parameter_files():
+    test_data_input_dir = Path(__file__).parent.parent.parent.parent / 'test' / 'data' / 'input'
+    test_data_output_dir = Path(__file__).parent.parent.parent.parent / 'test' / 'data' / 'output'
+    test_data_output_dir.mkdir(parents=True, exist_ok=True)
+
+    parameters_multiple_file = test_data_input_dir / 'parameters_multiple.json'
+    with open(parameters_multiple_file, 'r') as f:
+        parameters_multiple = json.load(f)
+
+    translation_file = test_data_output_dir / 'translation.txt'
+    affine_file = test_data_output_dir / 'affine.txt'
+
+    write_parameter_files(parameters_multiple, [translation_file, affine_file])
