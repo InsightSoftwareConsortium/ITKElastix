61205ebbbf18c4c621244e762e974079715e1532
Matt McCormick <matt@mmmccormick.com>
2025-06-24 15:22:43 -0400

    ENH: Add transformix generated typescript sources
    Easier to track changes.

---
+++ wasm/typescript/src/transformix-node-options.ts
@@ -0,0 +1,18 @@
+// Generated file. To retain edits, remove this comment.
+
+interface TransformixNodeOptions {
+  /** Output image origin. */
+  outputOrigin?: number[]
+
+  /** Output image spacing. */
+  outputSpacing?: number[]
+
+  /** Output image size. */
+  outputSize?: number[]
+
+  /** Output image orientation direction matrix. */
+  outputDirection?: number[]
+
+}
+
+export default TransformixNodeOptions
